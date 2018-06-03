e27ad710a019eefe9cdc528599d33bc41c36cb5c
Marshall <mngordon@umich.edu>
2018-06-03 16:31:21 -0400

    added banner image and event details to home tab

--- lib/TourneyTabs/HomeTab.dart
+++ lib/TourneyTabs/HomeTab.dart
@@ -1,9 +1,58 @@
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget {
+  final Map _json;
+
+  HomeTab(this._json);
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    // TODO: implement banner ui build
+    bool hasBanner = false;
+    int bannerNum;
+    for (int i = 0; i < _json['images'].length; i++) {
+      if (_json['images'][i]['type'] == 'banner') {
+        hasBanner = true;
+        bannerNum = i;
+      }
+    }
+    //there is a banner image, which should be the top
+    if (hasBanner) {
+      return new ListView(
+        children: <Widget>[
+          new Image.network(_json['images'][bannerNum]['url']),
+          new Padding(
+            padding: EdgeInsets.all(8.0),
+          ),
+          new Text(
+            "Details",
+            style: new TextStyle(
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          new Padding(
+              padding: EdgeInsets.symmetric(horizontal: 6.0),
+              child: new Center(
+              child: Text(
+            _json['details'],
+            style: new TextStyle(
+              fontSize: 18.0,
+            ),
+          ))),
+          new Text(
+            "Location",
+            style: new TextStyle(
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      );
+    } else {
+      return new Text("HOME");
+    }
   }
+
 }
