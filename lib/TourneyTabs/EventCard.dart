c25d61d77ab37b3c91c75e4b0da3072143a5b4c1
Marshall <mngordon@umich.edu>
2018-06-05 20:18:41 -0400

    Started events tab

---
+++ lib/TourneyTabs/EventCard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:smash_gg/api.dart';
+
+class EventCard extends StatelessWidget{
+  final Map _json;
+
+  EventCard(this._json);
+
+  @override
+  Widget build(BuildContext context){
+    return new Text(" ");
+  }
+}
--- lib/TourneyTabs/EventTab.dart
+++ lib/TourneyTabs/EventTab.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 class EventTab extends StatelessWidget {
+  final Map _json;
+
+  EventTab(this._json);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Text(" ");
+    return new Text("Events");
   }
 }
