f0cdba86963e26ceadfbb092f568e49b0aaa5a9e
Marshall <mngordon@umich.edu>
2018-06-16 18:33:47 -0400

    Added standings to completed event cards

--- lib/TourneyTabs/AttendeesTab.dart
+++ lib/TourneyTabs/AttendeesTab.dart
@@ -3,7 +3,15 @@ import 'package:flutter/material.dart';
 class AttendeesTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Text(" ");
+    return new Column(
+      children: <Widget>[
+        new Row(
+          children: <Widget>[
+            new FlatButton(onPressed: (){}, child: Text('Filters')),
+            new Flexible(child: TextField(),),
+          ],
+        ),
+      ],
+    );
   }
 }
--- lib/TourneyTabs/EventCard.dart
+++ lib/TourneyTabs/EventCard.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:smash_gg/api.dart';
+import 'dart:async';
+import 'package:url_launcher/url_launcher.dart';
+
+const url = 'https://www.smash.gg/';
 
 class EventCard extends StatelessWidget {
   final Map _json;
+  //final Future<Null> launched;
+
 
   EventCard(this._json);
 
@@ -11,7 +17,9 @@ class EventCard extends StatelessWidget {
     final Api _api = new Api();
     return new Card(
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          Future<Null> laucnhed = _launchInBrowser(url + _json['slug']);
+        },
         highlightColor: Theme.of(context).accentColor,
         splashColor: Theme.of(context).accentColor,
         child: Container(
@@ -25,7 +33,7 @@ class EventCard extends StatelessWidget {
                   new Flexible(child: Text(
                     _json['name'],
                     textAlign: TextAlign.center,
-                    overflow: TextOverflow.clip,
+                    overflow: TextOverflow.ellipsis,
                     style: new TextStyle(
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
@@ -58,7 +66,21 @@ class EventCard extends StatelessWidget {
       ),
     );
   }
+  Future<Null> _launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceSafariVC: false, forceWebView: false);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
+  Future<Null> _launchInWebViewOrVC(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceSafariVC: true, forceWebView: true);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
   ///Creates the entrants box based on the tourney state
   ///
   ///Entrants box is a list of the top 10 entrants if tourney is unfinished.
@@ -67,7 +89,59 @@ class EventCard extends StatelessWidget {
   Widget entrantsBox(Api _api) {
     //tourney is done and standings should be shown
     if (_json['state'] == 3) {
-      return new Text(" ");
+      return new FutureBuilder(
+          future: _api.getTopPlacers(_json['slug']),
+          builder: (context, snapshot) {
+            if (snapshot.hasData){
+              if (snapshot.data.length == 0){
+                return new Text(" ");
+              }
+              String text = '';
+              // loop through all top attendees
+              for (int i = 0; i < 10 && i < snapshot.data.length; i++){
+                if (i == 5 || i == 7 || i ==9){
+                  text += (i).toString();
+                }
+                else {
+                  text += (i+1).toString();
+                }
+
+                text += ': ';
+                //see if there is only one player per participant
+                if(snapshot.data[i]['players'].length == 1){
+                  //see if there is a sponsor
+                  if (snapshot.data[i]['players'][0]['prefix'] != null &&  snapshot.data[i]['players'][0]['prefix'] != ""){
+                    text += snapshot.data[i]['players'][0]['prefix'];
+                    text += '|';
+                  }
+                  text += snapshot.data[i]['players'][0]['gamerTag'];
+                  text += '\n';
+                }
+                else {
+                  for (int j = 0; j < snapshot.data[i]['players'].length; j++){
+                    if (snapshot.data[i]['players'][j]['prefix'] != null &&  snapshot.data[i]['players'][j]['prefix'] != ""){
+                      text += snapshot.data[i]['players'][j]['prefix'];
+                      text += '|';
+                    }
+                    text += snapshot.data[i]['players'][j]['gamerTag'];
+                    if (j != snapshot.data[i]['players'].length -1) {
+                      text += '/';
+                    }
+                  }
+                  text += '\n';
+                }
+
+              }
+              return new Text(text,
+                textAlign: TextAlign.left,);
+            }
+            else if (snapshot.hasError){
+              return new Text("${snapshot.error}");
+            }
+            else {
+              return new Text(" ");
+            }
+          });
     }
     //tourney is still ongoing and should display yhe top 10 seeds
     else {
